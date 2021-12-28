c1d39eb5616d0092d4f6c299d6f10d191ebc2a83
Shijie Zheng <90960767+Jackzhengshijie@users.noreply.github.com>
2021-12-28 11:11:25 +0800

    Add files via upload

---
+++ VideoGamesSales/app.R
@@ -0,0 +1,13 @@
+source("global.R")
+
+shinyApp(
+  ui = dashboardPage(
+    header = dash_header()
+    , sidebar = dash_side_bar()
+    , body = dash_body()
+  ),
+  server = function(input, output, session){ 
+    callModule(easy_server, "easy")
+    callModule(hard_server, "hard")
+  }
+)
